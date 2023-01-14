6a1bde7915200ab9fdc2c6023734456475deff43
AndreySh <shingaryovan@gmail.com>
2023-01-14 13:06:23 +1200

    example 3 redacted, all work fine

--- exaple3_sum/Program.cs
+++ exaple3_sum/Program.cs
@@ -1,5 +1,7 @@
-﻿Console.WriteLine("Enter Number A");
-int numA = Console.readline();
-Console.WriteLine("Enter Number B");
-int numB = Console.readline();
-Console.WriteLine(numA + numB);
+﻿int numA = new Random().Next(1,100);
+int numB = new Random().Next(1,100);
+Console.Write(numA);
+Console.Write(" + ");
+Console.Write(numB);
+Console.Write(" = ");
+Console.Write(numA + numB);
